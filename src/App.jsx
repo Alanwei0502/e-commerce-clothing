17fbc4efdd454fb8ace47ec2b9f1edd5511add74
Alanweiwei <alans40609@gmail.com>
2021-11-24 23:15:53 +0800

    update our menu-items to navigate with router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
 import "./App.styles.scss";
 
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+    </Routes>
   );
 }
 
--- src/components/directory/Directory.jsx
+++ src/components/directory/Directory.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useMatch } from "react-router";
 import MenuItem from "../menu-item/MenuItem";
 import "./Directory.styles.scss";
 
@@ -7,33 +8,33 @@ const sections = [
     title: "hats",
     imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     id: 1,
-    linkUrl: "shop/hats",
+    linkUrl: "hats",
   },
   {
     title: "jackets",
     imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     id: 2,
-    linkUrl: "shop/jackets",
+    linkUrl: "jackets",
   },
   {
     title: "sneakers",
     imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     id: 3,
-    linkUrl: "shop/sneakers",
+    linkUrl: "sneakers",
   },
   {
     title: "womens",
     imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     size: "large",
     id: 4,
-    linkUrl: "shop/womens",
+    linkUrl: "womens",
   },
   {
     title: "mens",
     imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     size: "large",
     id: 5,
-    linkUrl: "shop/mens",
+    linkUrl: "mens",
   },
 ];
 
@@ -42,8 +43,8 @@ const Directory = () => {
 
   return (
     <div className="directory-menu">
-      {sections.map((sec) => (
-        <MenuItem key={sec.id} {...sec} />
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
       ))}
     </div>
   );
