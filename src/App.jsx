d84ea68db31ddcf4087d455757e04e30e70d12e0
Alanweiwei <alans40609@gmail.com>
2021-11-28 22:27:35 +0800

    integrated firebase into application, created sign in page with sign in component and sign in with google

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { auth } from "./firebase/firebase.utils";
+
+import Header from "./components/header/Header";
 import HomePage from "./pages/home/HomePage";
 import ShopPage from "./pages/shop/ShopPage";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/SignInAndSignUpPage";
+
 import "./App.styles.scss";
-import Header from "./components/header/Header";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    console.log(currentUser);
+
+    let unsubscribeFromAuth = null;
+    unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+    });
+
+    return () => unsubscribeFromAuth();
+  }, [currentUser]);
+
   return (
     <div className="app">
-      <Header />
+      <Header currentUser={currentUser} />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
-        {/* <Route path="/contact" element={<ContactPage />} /> */}
+        <Route path="/signin" element={<SignInAndSignUpPage />} />
       </Routes>
     </div>
   );
---
+++ src/components/sign-in/SignIn.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import FormInput from "../form-input/FormInput";
+import CustomButton from "../custom-button/CustomButton";
+import { signInWithGoogle } from "../../firebase/firebase.utils";
+import "./SignIn.styles.scss";
+
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setEmail("");
+    setPassword("");
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  return (
+    <div className="sign-in">
+      <h2 className="title"> I already have an account</h2>
+      <span>Sign in with your email and password</span>
+
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          required
+          label="email"
+          type="email"
+          name="email"
+          value={email}
+          handleChange={handleChangeEmail}
+        />
+        <FormInput
+          required
+          label="password"
+          type="password"
+          name="password"
+          value={password}
+          handleChange={handleChangePassword}
+        />
+        <div className="buttons">
+          <CustomButton type="submit">Sign In</CustomButton>
+          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            Sign In With Google
+          </CustomButton>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
