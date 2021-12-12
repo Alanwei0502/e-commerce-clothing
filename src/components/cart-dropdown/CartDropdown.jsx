fb922eae58647e572e48c0b5b17f4e73c7a939c0
Alanweiwei <alans40609@gmail.com>
2021-12-13 00:17:18 +0800

    created cart-item component and connected our cart dropdownwith cartItems reducer

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import CustomButton from "../custom-button/CustomButton.jsx";
+import CartItem from "../cart-item/CartItem.jsx";
 import "./CartDropdown.styles.scss";
+import { useSelector } from "react-redux";
 
 const CartDropdown = () => {
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  console.log({ cartItems });
   return (
     <div className="cart-dropdown">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
