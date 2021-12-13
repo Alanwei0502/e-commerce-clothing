9f8838bc6f91b306c598dfd3efb75a8f1e6f15f2
Alanweiwei <alans40609@gmail.com>
2021-12-14 00:47:31 +0800

    added addItem and removeItem functionality to checkout item component

--- src/components/chekout-item/CheckoutItem.jsx
+++ src/components/chekout-item/CheckoutItem.jsx
@@ -1,16 +1,47 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from "../../redux/cart/cart.actions";
 import "./CheckoutItem.styles.scss";
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
+const CheckoutItem = ({ cartItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  const dispatch = useDispatch();
+
+  const addItemHandler = () => {
+    dispatch(addItem(cartItem));
+  };
+
+  const removeItemHandler = () => {
+    dispatch(removeItem(cartItem));
+  };
+
+  const onRemoveHandler = () => {
+    dispatch(clearItemFromCart(cartItem));
+  };
+
   return (
     <div className="checkout-item">
       <div className="image-container">
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={removeItemHandler}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={addItemHandler}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
-      <div className="remove-button">&#10005;</div>
+      <div className="remove-button" onClick={onRemoveHandler}>
+        &#10005;
+      </div>
     </div>
   );
 };
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,5 +1,5 @@
 import { CartActionTypes } from "./cart.types";
-const { TOGGLE_CART_HIDDEN, ADD_ITEM } = CartActionTypes;
+const { TOGGLE_CART_HIDDEN, ADD_ITEM, CLEAR_ITEM_FROM_CART, REMOVE_ITEM } = CartActionTypes;
 
 export const toggleCartHidden = () => ({
   type: TOGGLE_CART_HIDDEN,
@@ -7,5 +7,15 @@ export const toggleCartHidden = () => ({
 
 export const addItem = (item) => ({
   type: ADD_ITEM,
+  item
+})
+
+export const clearItemFromCart = (item) => ({
+  type: CLEAR_ITEM_FROM_CART,
+  item
+})
+
+export const removeItem = (item) => ({
+  type: REMOVE_ITEM,
   item
 })
